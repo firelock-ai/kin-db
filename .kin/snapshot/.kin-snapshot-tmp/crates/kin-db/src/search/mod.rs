b68ccb3abfcd6454c859abfd8c2ece052cf9295b
Troy Fortin <troy@firelock.ai>
2026-03-25 02:44:32 -0400

    chore: add .kin/ to gitignore, remove accidentally committed kin data
    Signed-off-by: Troy Fortin <troy@firelock.ai>

--- .kin/snapshot/.kin-snapshot-tmp/crates/kin-db/src/search/mod.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2026 Firelock, LLC
-
-mod text;
-
-pub use text::TextIndex;
--- .kin/snapshot/.kin-snapshot-tmp/crates/kin-db/src/vector/mod.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2026 Firelock, LLC
-
-mod hnsw;
-
-pub use hnsw::VectorIndex;
