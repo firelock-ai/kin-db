83caf6feb62a84b96ed0392984885940470f1b46
Troy Fortin <troy@firelock.io>
2026-03-25 02:44:32 -0400

    chore: add .kin/ to gitignore, remove accidentally committed kin data

--- .kin/snapshot/crates/kin-db/src/store.rs
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2026 Firelock, LLC
-
-//! Re-export of kin-model's GraphStore trait.
-//!
-//! kin-db now implements kin-model's GraphStore directly.
-
-pub use kin_model::GraphStore;
